6e78ff393160f0865de289e547479f7f4bcf0160
Alejandro Rojas <4lejandr0rojas@protonmail.com>
2023-01-08 10:03:20 -0600

    Sec 5: Resilience4J - p43. Configurando microservicio items con Resilience4J.

--- src/main/java/com/formacionbdi/springboot/app/item/springbootservicioitem/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/springbootservicioitem/controllers/ItemController.java
@@ -3,10 +3,10 @@ package com.formacionbdi.springboot.app.item.springbootservicioitem.controllers;
 import com.formacionbdi.springboot.app.item.springbootservicioitem.models.Item;
 import com.formacionbdi.springboot.app.item.springbootservicioitem.models.Producto;
 import com.formacionbdi.springboot.app.item.springbootservicioitem.models.service.ItemService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +14,9 @@ import java.util.List;
 @RestController
 @Slf4j
 public class ItemController {
+
+    @Autowired
+    private CircuitBreakerFactory cbFactory;
     @Autowired
     @Qualifier("serviceFeign")
     private ItemService itemService;
@@ -25,10 +28,11 @@ public class ItemController {
         return itemService.findAll();
     }
 
-    @HystrixCommand(fallbackMethod = "metodoAlternativo")
+    //@HystrixCommand(fallbackMethod = "metodoAlternativo")
     @GetMapping("/ver/{id}/cantidad/{cantidad}")
     public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad){
-        return itemService.findById(id, cantidad);
+        return cbFactory.create("items")
+                .run(() -> itemService.findById(id, cantidad), e -> metodoAlternativo(id, cantidad));
     }
 
     public Item metodoAlternativo(Long id, Integer cantidad) {
